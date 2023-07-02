0c80ec848790890a563a6511d82a8aa57a05adee
imlnr <bablureddy553@gmail.com>
2023-07-02 17:59:29 +0530

    final Commit

--- setoutput.py
+++ setoutput.py
@@ -1,20 +1,37 @@
-n = int(input())
-sett = set(map(int,input().split()[0:n]))
-print(sett)
-operations = int(input())
-for i in range(operations):
-    data = list(map(str, input().split()))
+def setoperations(sett,operations):
+    for _ in range(operations):
+        operation = input().split()
 
-    if data[0] == 'pop' or data[0] =='POP' or data[0] =='Pop':
-        sett.pop()
-        print(sett)
-    
-    elif data[0] == 'remove' or data[0] =='Remove':
-        sett.remove(data[1])
-        print(sett)
-    
-    elif data[0] == 'discard' or data[0] =='Discard':
-        sett.discard(data[1])
-        print(sett)
-    else:
-        print("Key error:")
+        if operation[0] == 'pop':
+            if sett:
+                sett.pop()
+            else:
+                return "Error: Set is empty"
+
+        elif operation[0] == 'remove':
+            if len(operation) != 2:
+                return "Error: Invalid input"
+            else:
+                element = int(operation[1])
+                try:
+                    sett.remove(element)
+                except KeyError:
+                    return "Error: Element not found in set"
+
+        elif operation[0] == 'discard':
+            if len(operation) != 2:
+                return "Error: Invalid input"
+            else:
+                element = int(operation[1])
+                sett.discard(element)
+
+        else:
+            return "Error: Invalid operation"
+        
+    return sett
+
+if __name__ == "__main__":
+    n = int(input())
+    sett = set(map(int, input().split()))
+    operations = int(input())
+    print(setoperations(sett,operations))
